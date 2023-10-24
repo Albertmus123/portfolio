76a6f223edc9a9df9a31e32aa70dc2343f0bd858
Albertmus123 <mualbert2@gmail.com>
2023-10-24 17:25:44 +0200

    add: stylin

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export const Nav = () => {
   return (
     <>
-<nav class="flex justify-end space-x-4 py-4 px-8 sticky top-0 bg-sky-500">
+<nav class="flex justify-end space-x-4 py-4 px-8">
   <a href="/team" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
   <a href="/team" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
   <a href="/projects" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
